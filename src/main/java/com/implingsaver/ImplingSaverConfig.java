d4ebb26e4d76453263130870caa66cb57e2f64d5
Nathan <nathan.collaert@gmail.com>
2023-06-09 18:51:44 +0200

    Add option to set 1 tick cooldown on opening jars
    Add option to set a 1 tick cooldown on opening jars so you cannot spam click open more than 1 jar per tick and in turn also not open more jars if a clue scroll is gotten.

--- src/main/java/com/implingsaver/ImplingSaverConfig.java
+++ src/main/java/com/implingsaver/ImplingSaverConfig.java
@@ -5,50 +5,58 @@ import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
 @ConfigGroup("ImplingSaver")
-public interface ImplingSaverConfig extends Config
-{
-	@ConfigItem(
-			keyName = "beginner",
-			name = "Beginner Clues",
-			description = "Young and baby impling jars will be hidden when beginner clue present"
-	)
-	default boolean beginnerMode() {
-		return true;
-	}
+public interface ImplingSaverConfig extends Config {
+    @ConfigItem(
+            keyName = "beginner",
+            name = "Beginner Clues",
+            description = "Young and baby impling jars will be hidden when beginner clue present"
+    )
+    default boolean beginnerMode() {
+        return true;
+    }
 
-	@ConfigItem(
-		keyName = "easy",
-		name = "Easy Clues",
-		description = "Gourmet and young impling jars will be hidden when easy clue present"
-	)
-	default boolean easyMode() {
-		return true;
-	}
+    @ConfigItem(
+            keyName = "easy",
+            name = "Easy Clues",
+            description = "Gourmet and young impling jars will be hidden when easy clue present"
+    )
+    default boolean easyMode() {
+        return true;
+    }
 
-	@ConfigItem(
-			keyName = "medium",
-			name = "Medium Clues",
-			description = "Eclectic and essence impling jars will be hidden when medium clue present"
-	)
-	default boolean mediumMode() {
-		return true;
-	}
+    @ConfigItem(
+            keyName = "medium",
+            name = "Medium Clues",
+            description = "Eclectic and essence impling jars will be hidden when medium clue present"
+    )
+    default boolean mediumMode() {
+        return true;
+    }
 
-	@ConfigItem(
-			keyName = "hard",
-			name = "Hard Clues",
-			description = "Ninja, magpie, and nature impling jars will be hidden when hard clue present"
-	)
-	default boolean hardMode() {
-		return true;
-	}
+    @ConfigItem(
+            keyName = "hard",
+            name = "Hard Clues",
+            description = "Ninja, magpie, and nature impling jars will be hidden when hard clue present"
+    )
+    default boolean hardMode() {
+        return true;
+    }
 
-	@ConfigItem(
-			keyName = "elite",
-			name = "Elite Clues",
-			description = "Dragon and crystal impling jars will be hidden when elite clue present"
-	)
-	default boolean eliteMode() {
-		return true;
-	}
+    @ConfigItem(
+            keyName = "elite",
+            name = "Elite Clues",
+            description = "Dragon and crystal impling jars will be hidden when elite clue present"
+    )
+    default boolean eliteMode() {
+        return true;
+    }
+
+    @ConfigItem(
+            keyName = "jarCooldown",
+            name = "Add open cooldown",
+            description = "Add a cooldown to opening Impling Jars so you can spam click."
+    )
+    default boolean jarCooldown() {
+        return true;
+    }
 }
--- src/test/java/com/implingsaver/ImplingSaverTest.java
+++ src/test/java/com/implingsaver/ImplingSaverTest.java
@@ -3,11 +3,9 @@ package com.implingsaver;
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ImplingSaverTest
-{
-	public static void main(String[] args) throws Exception
-	{
-		ExternalPluginManager.loadBuiltin(ImplingSaverPlugin.class);
-		RuneLite.main(args);
-	}
+public class ImplingSaverTest {
+    public static void main(String[] args) throws Exception {
+        ExternalPluginManager.loadBuiltin(ImplingSaverPlugin.class);
+        RuneLite.main(args);
+    }
 }
