95f5345d3ff119190e3120574cb328f45b034c0c
thelope <thelopeosrs@gmail.com>
2025-01-12 17:54:21 -0500

    2.0: Block looting based on banked clues only

--- src/main/java/com/implingsaver/ImplingSaverConfig.java
+++ src/main/java/com/implingsaver/ImplingSaverConfig.java
@@ -1,62 +1,135 @@
+/*
+ * Copyright (c) 2024, TheLope <https://github.com/TheLope>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package com.implingsaver;
 
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
+import net.runelite.client.config.ConfigSection;
 
 @ConfigGroup("ImplingSaver")
-public interface ImplingSaverConfig extends Config {
+public interface ImplingSaverConfig extends Config
+{
+	String CONFIG_GROUP = "ImplingSaver";
+	String BEGINNER_BANKED = "beginnerBanked";
+	String EASY_BANKED = "easyBanked";
+	String MEDIUM_BANKED = "mediumBanked";
+	String HARD_BANKED = "hardBanked";
+	String ELITE_BANKED = "eliteBanked";
+
     @ConfigItem(
-            keyName = "beginner",
-            name = "Beginner Clues",
-            description = "Young and baby impling jars will be hidden when beginner clue present"
+		keyName = "beginner",
+		name = "Beginner Clues",
+		description = "Looting baby and young impling jars is blocked when beginner clue in bank",
+		position = 0
     )
-    default boolean beginnerMode() {
+    default boolean beginnerMode()
+	{
         return true;
     }
 
     @ConfigItem(
-            keyName = "easy",
-            name = "Easy Clues",
-            description = "Gourmet and young impling jars will be hidden when easy clue present"
+		keyName = "easy",
+		name = "Easy Clues",
+		description = "Looting baby, young, and gourmet impling jars is blocked when easy clue in bank",
+		position = 1
     )
-    default boolean easyMode() {
+    default boolean easyMode()
+	{
         return true;
     }
 
     @ConfigItem(
-            keyName = "medium",
-            name = "Medium Clues",
-            description = "Eclectic and essence impling jars will be hidden when medium clue present"
+		keyName = "medium",
+		name = "Medium Clues",
+		description = "Looting earth, eclectic, and essence impling jars is blocked when medium clue in bank",
+		position = 2
     )
-    default boolean mediumMode() {
+    default boolean mediumMode()
+	{
         return true;
     }
 
     @ConfigItem(
-            keyName = "hard",
-            name = "Hard Clues",
-            description = "Ninja, magpie, and nature impling jars will be hidden when hard clue present"
+		keyName = "hard",
+		name = "Hard Clues",
+		description = "Looting nature, magpie, and ninja impling jars is blocked when hard clue in bank",
+		position = 3
     )
-    default boolean hardMode() {
+    default boolean hardMode()
+	{
         return true;
     }
 
     @ConfigItem(
-            keyName = "elite",
-            name = "Elite Clues",
-            description = "Dragon and crystal impling jars will be hidden when elite clue present"
+		keyName = "elite",
+		name = "Elite Clues",
+		description = "Looting crystal and dragon impling jars is blocked when elite clue in bank",
+		position = 4
     )
-    default boolean eliteMode() {
+    default boolean eliteMode()
+	{
         return true;
     }
 
-    @ConfigItem(
-            keyName = "jarCooldown",
-            name = "Add open cooldown",
-            description = "Add a cooldown to opening Impling Jars so you can spam click."
-    )
-    default boolean jarCooldown() {
-        return false;
-    }
+	@ConfigSection(name = "Overlays", description = "Options that effect overlays", position = 5)
+	String overlaysSection = "Overlays";
+
+	@ConfigItem(
+		keyName = "showChatMessage",
+		name = "Show chat message",
+		description = "Show chat message indicating when implings are being saved",
+		section = overlaysSection,
+		position = 0
+	)
+	default boolean showChatMessage()
+	{
+		return true;
+	}
+
+	@ConfigItem(
+		keyName = "showInfobox",
+		name = "Show infobox",
+		description = "Show infobox indicating when implings are being saved",
+		section = overlaysSection,
+		position = 1
+	)
+	default boolean showInfobox()
+	{
+		return false;
+	}
+
+	@ConfigItem(
+		keyName = "showTooltip",
+		name = "Show tooltip",
+		description = "Show tooltip on implings \"Loot\" hover when implings are being saved",
+		section = overlaysSection,
+		position = 2
+	)
+	default boolean showTooltip()
+	{
+		return true;
+	}
 }
---
+++ src/main/java/com/implingsaver/ImplingSaverOverlay.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2024, TheLope <https://github.com/TheLope>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.implingsaver;
+
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import javax.inject.Inject;
+import net.runelite.api.Client;
+import net.runelite.api.Menu;
+import net.runelite.api.MenuEntry;
+import net.runelite.client.ui.overlay.OverlayLayer;
+import net.runelite.client.ui.overlay.OverlayPanel;
+import net.runelite.client.ui.overlay.OverlayPosition;
+import net.runelite.client.ui.overlay.tooltip.Tooltip;
+import net.runelite.client.ui.overlay.tooltip.TooltipManager;
+
+public class ImplingSaverOverlay extends OverlayPanel
+{
+	private final Client client;
+	private final ImplingSaverPlugin plugin;
+	private final ImplingSaverConfig config;
+	private final TooltipManager tooltipManager;
+
+	@Inject
+	public ImplingSaverOverlay(Client client, ImplingSaverPlugin plugin, ImplingSaverConfig config, TooltipManager tooltipManager)
+	{
+		this.client = client;
+		this.plugin = plugin;
+		this.config = config;
+		this.tooltipManager = tooltipManager;
+
+		setPosition(OverlayPosition.TOOLTIP);
+		setLayer(OverlayLayer.ABOVE_WIDGETS);
+		setPriority(PRIORITY_HIGHEST);
+		setDragTargetable(false);
+	}
+
+	@Override
+	public Dimension render(Graphics2D graphics)
+	{
+		renderMouseover();
+		return null;
+	}
+
+	private void renderMouseover()
+	{
+		if (!config.showTooltip()) return;
+
+		Menu menu = client.getMenu();
+		MenuEntry[] menuEntries = menu.getMenuEntries();
+		if (menuEntries.length == 0)
+		{
+			return;
+		}
+
+		MenuEntry entry = menuEntries[menuEntries.length - 1];
+		String menuOption = entry.getOption();
+		int itemId = entry.getItemId();
+
+		if (plugin.isImplingToSave(itemId) && menuOption.equals("Loot"))
+		{
+			String tooltipText = plugin.getCause(itemId);
+
+			if (tooltipText != null){
+				tooltipManager.add(new Tooltip(tooltipText));
+			}
+		}
+	}
+}
